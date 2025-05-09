358e1ae492a7f0b13ff5b03f49591b028d81264f
muhammad-hamza-liaqat <mh408800@gmail.com>
2025-05-09 12:55:12 +0500

    fix: remove unnecessary comment from CSV download URL

--- server/riot/urls.py
+++ server/riot/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('', views.summoner_profile, name='profile_form'),
     path('get-riot-matches/', views.RiotMatchesView.as_view(), name='get_riot_matches'),
     path('match-details/', views.match_details, name='match_details'),
-    path('download-csv/', views.download_match_csv, name='download_match_csv'),  # New URL for CSV download
+    path('download-csv/', views.download_match_csv, name='download_match_csv'),
 ]
